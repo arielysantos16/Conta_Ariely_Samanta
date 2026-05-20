91915e1ccbe8fc926f1dccaea3e82cf07ab0abe6
Samanta Caroline <samantacarolinesantos@gmail.com>
2026-05-20 17:06:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from conta import Conta 
+obj_conta = Conta()
 opcao = int(input (" <3 Selecione o que deseja fazer:\n 1-Saque\n 2-Depositar\n 3-Calcular Rendimento"))
 if opcao == 1: 
-  
+  saque = int(input("Qual valor você deseja sacar?"))
+  obj_conta.sacar(saque)
+  mostrar = 
